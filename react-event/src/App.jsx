0e136ca6cff1b51525e499e8cc58b1860b8ea1de
lucas <lucas.vasquezfc@gmail.com>
2024-01-04 23:29:11 -0300

    react event e components

---
+++ react-event/src/App.jsx
@@ -0,0 +1,15 @@
+import Footer from './Footer';
+import Form from './Form/Form';
+import Header from './Header';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Form />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ react-event/src/Form/Form.jsx
@@ -0,0 +1,20 @@
+import Input from './input';
+import Button from './Button';
+
+const Form = () => {
+  return (
+    <form>
+      <p>
+        <label htmlFor="nome">Nome</label>
+        <Input />
+      </p>
+      <p>
+        <label htmlFor="mail">Email</label>
+        <Input />
+      </p>
+      <Button />
+    </form>
+  );
+};
+
+export default Form;
